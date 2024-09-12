dd75036ecd5a47166f2308954f214f5de43acee1
TheAppleMk12 <Abramhtran@gmail.com>
2024-09-12 11:23:53 -0700

    finished assinment

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,21 @@
+public class Problem3
+{
+    public static void main(String[] args)
+    {
+        double num1;
+        num1 = 67.3424;
+        int amount;
+        amount = 4;
 
+        System.out.println("Decimal number: " + num1);
+        System.out.print("Answer: ");
+
+        while (amount > 0){
+            int digit;
+            num1 = (num1 % 1) * 10;
+            digit = (int)(num1);
+            amount--;
+            System.out.print(digit + " ");
+        }
+    }
+}
