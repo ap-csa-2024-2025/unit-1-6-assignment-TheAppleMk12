666853d8c60cfbf4be5dd2d89ec55da27dc9e364
TheAppleMk12 <abramhtran@gmail.com>
2024-09-26 10:53:46 -0700

    Completed assignment

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,16 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    double num1 = 57.3934;
+    double num2 = 22.5211;
+    int rNum1 = (int)(num1 + 0.5);
+    int rNum2 = (int)(num2 + 0.5);
+    int result = rNum1 + rNum2;
 
+    System.out.println("Two original numbers: ");
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println("Answer: " + rNum1 + " + " + rNum2 + " = " + result);
+  }
+}
